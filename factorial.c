5c1e8c49bc8b71e0bf28d1bf515f81ef8d5b5780
Chandhru <64609696+chandhrukuslavan@users.noreply.github.com>
2024-05-24 15:05:48 +0530

    Finding of FActorial

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    flot fact=0.0,n,num;
+    printf("enter no");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+    fact=fact*i;
+int sum =sum+(num/fact);    
+num++;
+}
+
+printf("the value is :%d",sum);
+}
