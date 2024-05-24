952176e8b23889d20e5097463562e772a798da30
Chandhru <64609696+chandhrukuslavan@users.noreply.github.com>
2024-05-24 14:57:13 +0530

    Addition Of Array

---
+++ array_addition.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+int main()
+{
+      int a[10],i,n,sum=0;
+          printf("enter a nos below 10:");
+           scanf("%d",&n);
+       for(i=0;i<n;i++)
+{
+          printf("enter the value");
+            scanf("%d",&a[i]);
+              sum = sum + a[i];
+}
+printf ("enter the sum of nos\n");
+
+              for(i=1;i<n;i++)
+{      
+printf ("the sum of numers is :%d\n",sum);
+}}
+
