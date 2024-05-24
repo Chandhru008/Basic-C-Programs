dec02c7e29c62f1764fbda58b8398879efa6413e
Chandhru <64609696+chandhrukuslavan@users.noreply.github.com>
2024-05-24 15:03:45 +0530

    Checking whether the number is Positive or Negative Using Switch case

---
+++ positivenegative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+        {   
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    switch(n>0){
+case 1: 
+    printf("no is positive\n");
+break;
+}
+    switch(n<0){
+    case 1: 
+    printf("No is negative\n");
+    break;}
+switch(n==0)
+{
+        case 1 :
+    printf("the no is zero");
+break;
+}
+}
